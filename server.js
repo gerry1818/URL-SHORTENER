9149bf5162f252242c951026426bae72fa74ed54
gerry1818 <prajapatigirish07@gmail.com>
2020-05-13 18:14:34 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,102 @@
+'use strict';
+
+var express = require('express');
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+
+var os = require('os');
+
+var Schema = mongoose.Schema;
+var app = express();
+
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(cors());
+
+app.use('/public', express.static(process.cwd() + '/public'));
+app.set('view engine', 'ejs');
+
+var shortUrlSchema = new Schema({
+  url: { type: String },
+  shortenUrl: String
+});
+
+var shortUrl = mongoose.model('ShortUrl', shortUrlSchema);
+
+shortUrl.countDocuments(function (err, count) {
+  if (!err && count === 0) {
+    var urlobj = new shortUrl({ url: "temp", shortenUrl: "-1" });
+    urlobj.save();
+  }
+});
+
+
+var port = 3000;
+
+mongoose.connect("mongodb+srv://admin:admin@cluster0-eanzd.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.get('/', function (req, res) {
+  res.render("index");
+});
+
+
+
+
+app.post("/api/shorturl/new", function (req, res) {
+
+  const expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+  const url_valid = new RegExp(expression);
+
+  let url = req.body.url;
+  if (url.match(url_valid)) {
+
+    shortUrl.countDocuments(function (err, count) {
+      if (err) {
+        console.log(err);
+      }
+      else {
+        let urlobj = new shortUrl({
+          url: url,
+          shortenUrl: count.toString()
+        });
+
+        urlobj.save();
+        console.log("saved successfully");
+        res.render("response", {
+          urlobj, path: urlobj.shortenUrl
+        });
+      }
+    });
+  }
+  else {
+    res.json({
+      "error": "invalid URL"
+    });
+  }
+
+});
+
+
+app.get('/api/shorturl/:shortURL', function (req, res) {
+  var short_url = req.params.shortURL;
+
+  shortUrl.findOne({ shortenUrl: short_url }, function (err, obj) {
+    if (err) {
+      console.log(err);
+      res.json({ "error": "invalid URL" });
+    }
+    else {
+      res.redirect(obj.url);
+    }
+
+  })
+
+
+});
+
+app.listen(port, function () {
+  console.log('Node.js listening ...');
+});
